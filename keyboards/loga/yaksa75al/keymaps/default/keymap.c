f2c9ba045aaa22a2752a9cb086e71b1af07b2c7e
Vatunyoo Suwannapisit <ker.los@hotmail.com>
2024-11-09 02:51:20 +0700

    update keymap

--- keyboards/loga/yaksa75al/keymaps/default/keymap.c
+++ keyboards/loga/yaksa75al/keymaps/default/keymap.c
@@ -8,6 +8,7 @@ enum custom_layers {
     WIN_FN,
     MAC_BASE,
     MAC_FN,
+    UNKNOWN,
 };
 
 enum custom_keycodes {
@@ -29,15 +30,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSLS, KC_PGUP, 
         KC_CAPS, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT, KC_PGDN, 
         KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, KC_UP,  
-        KC_LCTL, KC_LGUI, KC_LALT, KC_SPC, KC_RALT, MO(1), KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
+        KC_LCTL, KC_LGUI, KC_LALT, KC_SPC, KC_RALT, MO(WIN_FN), KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
     ),
 
     [WIN_FN] = LAYOUT(
-        QK_CLEAR_EEPROM, KC_MSEL, KC_VOLD, KC_VOLU, KC_MUTE, KC_MSTP, KC_MPRV, KC_MPLY, KC_MNXT, KC_MAIL, KC_WWW_HOME, KC_CALC, KC_WWW_SEARCH, KC_INS,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, RGB_TOG, KC_END,
-        _______, CUSTOM(0), CUSTOM(1), CUSTOM(2), CUSTOM(5), _______, _______, _______, _______, MO(4), _______, _______, RGB_HUI, RGB_MOD, KC_PSCR,
-        _______,  TO(2), _______, _______, _______, _______, _______, _______, MO(4), _______, _______,  CUSTOM(26), KC_PAUS,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  MO(4), RGB_VAI,
+        QK_CLEAR_EEPROM, KC_MSEL, KC_VOLD, KC_VOLU, KC_MUTE, KC_MSTP, KC_MPRV, KC_MPLY, KC_MNXT, KC_MAIL, KC_WWW_HOME, KC_CALC, KC_WWW_SEARCH, KC_INS, KC_MUTE,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, RGB_TOG, KC_END,
+        _______, CUSTOM(0), CUSTOM(1), CUSTOM(2), CUSTOM(5), _______, _______, _______, _______, MO(UNKNOWN), _______, _______, RGB_HUI, RGB_MOD, KC_PSCR,
+        _______, _______,  DF(MAC_BASE), _______, _______, _______, _______, _______, _______, MO(UNKNOWN), _______, _______,  CUSTOM(26), KC_PAUS,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  MO(UNKNOWN), RGB_VAI,
         _______, 0x700b, _______,   CUSTOM(7),    _______, _______, _______, RGB_SPD, RGB_VAD, RGB_SPI
     )
     
@@ -51,11 +52,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
     
     [MAC_FN] = LAYOUT(
-       QK_CLEAR_EEPROM, KC_BRID, KC_BRIU, _______, _______, RGB_VAD, RGB_VAI, KC_MPRV, KC_MPLY, KC_MNXT, KC_MUTE, KC_VOLD, KC_VOLU, KC_INS,
-       _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, RGB_TOG, KC_END
-       _______, CUSTOM(0), CUSTOM(1), CUSTOM(2), CUSTOM(5), _______, _______, _______, _______, MO(4), _______, _______, RGB_HUI, RGB_MOD, KC_PSCR
-       _______, TO(0),  _______, _______, _______, _______, _______, _______, MO(4), _______, _______,  CUSTOM(26), KC_PAUS,
-       _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  MO(4), RGB_VAI,
+       QK_CLEAR_EEPROM, KC_BRID, KC_BRIU, _______, _______, RGB_VAD, RGB_VAI, KC_MPRV, KC_MPLY, KC_MNXT, KC_MUTE, KC_VOLD, KC_VOLU, KC_INS, KC_MUTE,
+       _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, RGB_TOG, KC_END,
+       _______, CUSTOM(0), CUSTOM(1), CUSTOM(2), CUSTOM(5), _______, _______, _______, _______, MO(UNKNOWN), _______, _______, RGB_HUI, RGB_MOD, KC_PSCR,
+       _______, DF(WIN_BASE), _______,  _______, _______, _______, _______, _______, _______, MO(UNKNOWN), _______, _______,  CUSTOM(26), KC_PAUS,
+       _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  MO(UNKNOWN), RGB_VAI,
        _______, _______, _______,   CUSTOM(7),    _______, _______, _______, RGB_SPD, RGB_VAD, RGB_SPI
     ),
     
